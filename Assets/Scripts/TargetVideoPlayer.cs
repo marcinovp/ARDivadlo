ead6d5da067847c4fb4afb27c450db11d576dd4c
psych0 <p.marcinov@gmail.com>
2018-12-20 23:06:40 +0100

    Naimportovane testovacie video, provizorny skript na prehravanie po najdeni image targetu

---
+++ Assets/Scripts/TargetVideoPlayer.cs
@@ -0,0 +1,42 @@
+﻿using EasyAR;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TargetVideoPlayer : MonoBehaviour
+{
+    public ImageTargetBaseBehaviour imageTargetBehaviour;
+
+    private VideoPlayer videoPlayer;
+    private MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        Debug.Log("Awake");
+        videoPlayer = GetComponent<VideoPlayer>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start");
+        imageTargetBehaviour.TargetFound += ImageTargetBehaviour_TargetFound;
+        imageTargetBehaviour.TargetLost += ImageTargetBehaviour_TargetLost;
+    }
+    
+    private void ImageTargetBehaviour_TargetLost(TargetAbstractBehaviour obj)
+    {
+        Debug.Log("Target lost");
+        videoPlayer?.Pause();
+        meshRenderer.enabled = false;
+    }
+
+    private void ImageTargetBehaviour_TargetFound(TargetAbstractBehaviour obj)
+    {
+        Debug.Log("Target found");
+        videoPlayer.Play();
+        meshRenderer.enabled = true;
+    }
+}
